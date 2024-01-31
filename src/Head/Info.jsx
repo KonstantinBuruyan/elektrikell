7d83c6d0a2b503088d6b8f2491387b50dcb6f274
Konstantin Buruyan <k.burujan@gmail.com>
2024-01-31 18:54:48 +0200

    Add current price

--- src/Head/Info.jsx
+++ src/Head/Info.jsx
@@ -3,14 +3,24 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { PRICE_BUTTONS, BADGES } from "./constants";
 import Badge from 'react-bootstrap/Badge';
+import { useEffect, useState } from 'react';
+import { getCurrentPrice } from '../services/apiService';
+import { mwToKw, addTax } from '../Utils/priceFormats';
 
-function Info({ activePrice, setActivePrice,  }) {
+function Info({ activePrice, setActivePrice, }) {
+    const [currentPrice, setCurrentPrice] = useState(0);
 
+    useEffect(() => {
+        (async () => {
+            const { data } = await getCurrentPrice();
+            setCurrentPrice(addTax(mwToKw(data[0].price),"ee"));
+        })()
+    }, []);
     return (<>
         <Col>
             <div>The current price of electricity is</div>
             <Badge bg={BADGES[0].name}>{BADGES[0].id}</Badge>
-      {/* <Badge bg="danger">Danger</Badge> */}
+            {/* <Badge bg="danger">Danger</Badge> */}
         </Col>
         <Col>
             <ButtonGroup >
@@ -22,8 +32,8 @@ function Info({ activePrice, setActivePrice,  }) {
             </ButtonGroup>
         </Col>
         <Col className='text-end'>
-        <h2>XX.XX</h2>
-        <div>cent / kilowatt-hour</div>
+            <h2>{currentPrice}</h2>
+            <div>cent / kilowatt-hour</div>
         </Col>
     </>);
 }
